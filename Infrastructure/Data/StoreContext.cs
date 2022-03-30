842d6a7ac23818e49a86f59f813fe71b4746733c
Zeeshan-ARCADIS <Mohd.Zeeshan@Arcadis.Com>
2022-03-30 17:07:38 +0530

    End of section 3

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,4 +9,12 @@ public class StoreContext : DbContext
     public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
 
     public DbSet<Product> Products { get; set; }
+    public DbSet<ProductType> ProductTypes { get; set; }
+    public DbSet<ProductBrand> ProductBrands { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
